3c555563851c962750004040a685af5c2fa1135a
ujjwaltyagi10 <ujjwaltyagi1015@gmail.com>
2026-06-24 13:03:12 +0530

    fix the theme color in navbar

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.6egca2f22qo"
+    "revision": "0.ictsi3192ng"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
